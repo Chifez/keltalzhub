22fdc9108cccf0a4a7f2a33e2cc71013690de422
emmanuel <chifez1@gmail.com>
2023-12-04 13:06:34 +0000

    made chnages to my alias

--- git_push.sh
+++ git_push.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+echo "Executing git_push.sh"
 git add .
+echo "Changes added"
 git commit -m "$1"
-git push origin master 
+echo "Commit created"nano ~/.bashrc
+git push origin master
+echo "Push completed"
