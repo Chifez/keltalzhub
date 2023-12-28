67a59cf7f3c4f10faeb58fa16b7db01304a1ea81
emmanuel <chifez1@gmail.com>
2023-12-28 21:26:22 +0000

    working on the brochure download

--- app/api/downloadBrochure/[filename]/route.ts
+++ app/api/downloadBrochure/[filename]/route.ts
@@ -27,7 +27,7 @@ const drive = google.drive({
   auth: 'NEXT_PUBLIC_GOOGLE_OAUTH',
 });
 
-export async function GET(req: NextApiRequest, res: NextApiResponse) {
+export async function POST(req: NextApiRequest, res: NextApiResponse) {
   try {
     const fileId = req.query.id as string;
 
