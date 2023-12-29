38d3eaaf66fa0d29df57de36c91213ef3a7c428d
emmanuel <chifez1@gmail.com>
2023-12-29 10:16:05 +0000

    brochure can now be previewed and dowloaded;i really need to make it doenloadabe directly

--- app/api/downloadBrochure/[filename]/route.ts
+++ app/api/downloadBrochure/[filename]/route.ts
@@ -27,34 +27,32 @@
 //   auth: 'NEXT_PUBLIC_GOOGLE_OAUTH',
 // });
 
-// export async function GET(req: any, res: any) {
-//   //   const fileId = req.query?.filename as string;
-//   const fileId = '1Ho6KhlLxZzni6EVPsMrQC8NHrNQW1qfh';
-//   console.log(fileId);
-//   if (!fileId) {
-//     console.log(fileId);
-//     return res.status(400).json({ error: 'File ID not provided' });
-//   }
-//   try {
-//     // Validate and sanitize fileId
-//     if (!isValidFileId(fileId)) {
-//       return res.status(400).json({ error: 'Invalid file ID' });
-//     }
-
-//     // Authorization check (example: check if the user is authenticated)
-
-//     const { data } = await drive.files.get(
-//       { fileId, alt: 'media' },
-//       { responseType: 'arraybuffer' }
-//     );
-//     res.setHeader('Content-Type', data.mimeType || 'application/octet-stream');
-//     res.setHeader('Content-Disposition', `attachment; filename="${data.name}"`);
-//     res.status(200).send(data);
-//   } catch (error) {
-//     console.error('Error downloading file from Google Drive:', error);
-//     res.status(500).json({ error: 'Internal Server Error' });
-//   }
-// }
+export async function GET(req: any, res: any) {
+  //   //   const fileId = req.query?.filename as string;
+  //   const fileId = '1Ho6KhlLxZzni6EVPsMrQC8NHrNQW1qfh';
+  //   console.log(fileId);
+  //   if (!fileId) {
+  //     console.log(fileId);
+  //     return res.status(400).json({ error: 'File ID not provided' });
+  //   }
+  //   try {
+  //     // Validate and sanitize fileId
+  //     if (!isValidFileId(fileId)) {
+  //       return res.status(400).json({ error: 'Invalid file ID' });
+  //     }
+  //     // Authorization check (example: check if the user is authenticated)
+  //     const { data } = await drive.files.get(
+  //       { fileId, alt: 'media' },
+  //       { responseType: 'arraybuffer' }
+  //     );
+  //     res.setHeader('Content-Type', data.mimeType || 'application/octet-stream');
+  //     res.setHeader('Content-Disposition', `attachment; filename="${data.name}"`);
+  //     res.status(200).send(data);
+  //   } catch (error) {
+  //     console.error('Error downloading file from Google Drive:', error);
+  //     res.status(500).json({ error: 'Internal Server Error' });
+  //   }
+}
 
 // function isValidFileId(fileId: string): boolean {
 //   return /^[a-zA-Z0-9]{28,}$/i.test(fileId);
