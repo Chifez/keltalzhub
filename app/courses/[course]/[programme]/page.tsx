0a35337875a5bb247414bc519a5bab570e334b00
emmanuel <chifez1@gmail.com>
2023-12-31 18:03:06 +0000

    fix:updated the dynamic course content

--- app/courses/[course]/[programme]/page.tsx
+++ app/courses/[course]/[programme]/page.tsx
@@ -11,6 +11,9 @@ const page = ({
   params: { course: string; programme: string };
 }) => {
   const { course, programme } = params;
+  // const courseInfo = CourseList.flatMap((item) => item.list).find(
+  //   (c) => c.url === `/courses/${course}/${programme}`
+  // );
   const courseInfo = CourseList.flatMap((item) => item.list).find(
     (c) => c.url === `/courses/${course}/${programme}`
   );
@@ -49,7 +52,7 @@ const page = ({
   return (
     <>
       <div className="flex flex-col items-center justify-center">
-        <div className="w-[80%] rounded-lg overflow-hidden">
+        <div className="w-[90%] md:w-[80%] rounded-lg overflow-hidden">
           <div className="relative w-full h-[250px] ">
             <Image
               layout="fill"
@@ -63,7 +66,7 @@ const page = ({
             <p>{courseInfo?.title}</p>
           </div>
         </div>
-        <div className="w-[80%] md:w-auto my-4 grid md:grid-cols-2 md:gap-x-40 gap-y-10">
+        <div className="w-[90%] md:w-auto my-4 grid md:grid-cols-2 md:gap-x-40 gap-y-10">
           {courseInfo?.info.map((item, idx) => (
             <div key={idx} className="flex items-center justify-start ">
               <div className="">
@@ -75,12 +78,26 @@ const page = ({
             </div>
           ))}
         </div>
-        <div className="w-[80%]">
-          <h3 className="text-xl text-[#D9D9D9] font-medium mb-4">
-            who is this course meant for?
-          </h3>
-          <ul className="list-disc">
-            <li className="text-xl">
+        {courseInfo.audience && (
+          <div className="w-[90%] md:w-[80%]">
+            <h3 className="text-xl text-[#D9D9D9] font-medium mb-4">
+              who is this course meant for?
+            </h3>
+            {courseInfo.headerDesc && (
+              <p className="text-xl mb-4">{courseInfo.headerDesc}</p>
+            )}
+            <ul
+              className={`${
+                courseInfo.headerDesc ? 'ml-10' : 'mr-0'
+              } list-disc`}
+            >
+              {courseInfo.audience &&
+                courseInfo.audience.map((item, idx) => (
+                  <li className="text-xl" key={idx}>
+                    {item}
+                  </li>
+                ))}
+              {/* <li className="text-xl">
               This class is suitable for novice users of After Effects or
               individuals engaged in or aspiring to work in the video industry,
               or Adobe users desiring to expand their expertise in video
@@ -91,9 +108,10 @@ const page = ({
               individuals engaged in or aspiring to work in the video industry,
               or Adobe users desiring to expand their expertise in video
               enhancements.
-            </li>
-          </ul>
-        </div>
+            </li> */}
+            </ul>
+          </div>
+        )}
         <div className="flex flex-col item-center justify-center my-8 gap-2">
           <Button
             title="enroll for this course"
--- libs/data/courseData.ts
+++ libs/data/courseData.ts
@@ -16,6 +16,10 @@ export const CourseList = [
           { header: 'Class Type', content: 'Online' },
           { header: 'Tuition', content: '50,000' },
         ],
+        audience: [
+          'This class is suitable for novice users of After Effects or individuals engaged in or aspiring to work in the video industry, or Adobe users desiring to expand their expertise in video enhancements.',
+          'This class covers the fundamental notions and proficiencies indispensable for excelling in animation for motion graphics and visual enhancements, alongside advanced methodologies and tactics.',
+        ],
       },
       {
         title: 'UI/UX',
@@ -29,6 +33,10 @@ export const CourseList = [
           { header: 'Training Days', content: 'Tuesday-Friday' },
           { header: 'Class Type', content: 'Online' },
           { header: 'Tuition', content: '50,000' },
+        ],
+        audience: [
+          'This class is suitable for novice users of After Effects or individuals engaged in or aspiring to work in the video industry, or Adobe users desiring to expand their expertise in video enhancements.',
+          'This class covers the fundamental notions and proficiencies indispensable for excelling in animation for motion graphics and visual enhancements, alongside advanced methodologies and tactics.',
         ],
       },
       {
@@ -44,6 +52,15 @@ export const CourseList = [
           { header: 'Class Type', content: 'Online' },
           { header: 'Tuition', content: '50,000' },
         ],
+        headerDesc:
+          'This class is suitable for individuals who are new to design generally or those who possess fundamental knowledge and are eager to elevate their existing proficiency. Upon successful completion of this course, students will be expected to:',
+        audience: [
+          'Exhibit a comprehensive comprehension of the components of graphic design.',
+          'Comprehend and convey in the vernacular of graphic design',
+          'Utilize software such as Corel Draw/Illustrator and the Internet',
+          'Students will acquire problem-solving skills using critical thinking',
+          'Approach briefs from the real-world standpoint.',
+        ],
       },
       {
         title: 'Animations',
@@ -57,6 +74,12 @@ export const CourseList = [
           { header: 'Training Days', content: 'Tuesday-Friday' },
           { header: 'Class Type', content: 'Online' },
           { header: 'Tuition', content: '50,000' },
+        ],
+        audience: [
+          'Individuals who have a basic understanding of digital technologies and are interested in pursuing a career in UI/UX design.',
+          'Professionals who want to enhance their skills and knowledge in UI/UX design to improve their current job prospects',
+          'Web designers, graphic designers, developers, or other professionals involved in digital product development who want to expand their skill set.',
+          'Students or recent graduates looking to specialize in UI/UX design and seeking a comprehensive introduction to the field.',
         ],
       },
     ],
